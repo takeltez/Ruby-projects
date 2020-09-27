691ab56df796c7ed7a67626197436d0ef3e10286
takeltez <mountain.skier@yandex.ru>
2020-09-27 20:48:46 +0700

    fix bugs

--- lab1/lib/C_K_F_Converter.rb
+++ lab1/lib/C_K_F_Converter.rb
@@ -1,89 +1,95 @@
 class Convertor
-	 def convert
- 	end
+	def convert()
+	end
 end
 
 class Reader
-	 attr_accessor :temperature, :in_degree_type, :out_degree_type
 
-	 def read
- 		print('Enter temperature: ')
- 		@temperature = gets.chomp
- 
- 		print('Enter incoming degree type (C, K, F): ')
- 		@in_degree_type = gets.chomp.upcase
+	attr_accessor :temperature, :in_degree_type, :out_degree_type
 
- 		print('Enter outgoing degree type (C, K, F): ')
- 		@out_degree_type = gets.chomp.upcase
+	def read()
 
- 		if @temperature[/[a-zA-Z_]/]
- 
- 			puts("\nTemperature must be a numeric value: #{@temperature}")
- 			exit
- 		else
- 			@temperature = @temperature.to_f
- 		end
+		print("Enter temperature: ")
+		@temperature = gets().chomp()
+		
+		print("Enter incoming degree type (C, K, F): ")
+		@in_degree_type = gets().chomp().upcase()
 
- 		if (@in_degree_type != 'C') && (@in_degree_type != 'K') && (@in_degree_type != 'F')
- 
- 			puts("\nIncorrect degree type: #{@in_degree_type}")
- 			exit
- 
- 		end
+		print("Enter outgoing degree type (C, K, F): ")
+		@out_degree_type = gets().chomp().upcase()
 
- 		if (@out_degree_type != 'C') && (@out_degree_type != 'K') && (@out_degree_type != 'F')
- 
- 			puts("\nIncorrect degree type: #{@out_degree_type}")
- 			exit
- 
- 		end
- 	end
+		if (@temperature[/[a-zA-Z_]/])
+			
+			puts("\nTemperature must be a numeric value: #{@temperature}")
+			exit
+		else
+			@temperature = @temperature.to_f()
+		end
+
+		if (@in_degree_type != "C" and @in_degree_type != "K" and @in_degree_type != "F")
+			
+			puts("\nIncorrect degree type: #{@in_degree_type}")
+			exit
+			
+		end
+
+		if (@out_degree_type != "C" and @out_degree_type != "K" and @out_degree_type != "F")
+			
+			puts("\nIncorrect degree type: #{@out_degree_type}")
+			exit
+			
+		end
+	end
 end
 
 class Adapter < Convertor
-	 def initialize(reader)
- 		@reader = reader
- 	end
 
-	 def convert
- 		case (@reader.in_degree_type)
-   when ('C')
- 				case (@reader.out_degree_type)
-     when ('C')
- 						[@reader.temperature, @reader.out_degree_type]
-     when ('K')
- 						(@reader.temperature + 273), @reader.out_degree_type
-     when ('F')
- 						(@reader.temperature * 1.8 + 32), @reader.out_degree_type
- 				end
-   when ('K')
- 				case (@reader.out_degree_type)
-     when ('C')
- 						(@reader.temperature - 273), @reader.out_degree_type
-     when ('K')
- 						[@reader.temperature, @reader.out_degree_type]
-     when ('F')
- 						(@reader.temperature * 1.8 - 459), @reader.out_degree_type
- 				end
-   when ('F')
- 				case (@reader.out_degree_type)
-     when ('C')
- 						((@reader.temperature - 32) / 1.8), @reader.out_degree_type
-     when ('K')
- 						((@reader.temperature + 459) / 1.8), @reader.out_degree_type
-     when ('F')
- 						[@reader.temperature, @reader.out_degree_type]
- 				end
- 		end
- 	end
+	def initialize(reader)
+
+		@reader = reader
+	end
+
+	def convert()
+
+		case(@reader.in_degree_type())
+			when("C")
+				case(@reader.out_degree_type())
+					when("C")
+						return @reader.temperature(), @reader.out_degree_type()
+					when("K")
+						return (@reader.temperature() + 273), @reader.out_degree_type()
+					when("F")
+						return (@reader.temperature() * 1.8 + 32), @reader.out_degree_type()	
+				end
+			when("K")
+				case(@reader.out_degree_type())
+					when("C")
+						return (@reader.temperature() - 273), @reader.out_degree_type()
+					when("K")
+						return @reader.temperature(), @reader.out_degree_type()
+					when("F")
+						return (@reader.temperature() * 1.8 - 459), @reader.out_degree_type()	
+				end
+			when("F")
+				case(@reader.out_degree_type())
+					when("C")
+						return ((@reader.temperature() - 32) / 1.8), @reader.out_degree_type()
+					when("K")
+						return ((@reader.temperature() + 459) / 1.8), @reader.out_degree_type()
+					when("F")
+						return @reader.temperature(), @reader.out_degree_type()
+				end			
+		end
+	end
 end
 
-reader = Reader.new
 
-reader.read
+reader = Reader.new()
+
+reader.read()
 
 adapter = Adapter.new(reader)
 
-degree = adapter.convert
+degree = adapter.convert()
 
 puts("\nTemperature (#{degree[1]}): #{degree[0].round(2)}")
