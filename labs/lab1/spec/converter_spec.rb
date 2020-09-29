890f4f78074f6375fb5b772df778a87fdebc9d30
takeltez <mountain.skier@yandex.ru>
2020-09-30 02:08:33 +0700

    Add context for tests

--- labs/lab1/spec/converter_spec.rb
+++ labs/lab1/spec/converter_spec.rb
@@ -2,32 +2,40 @@ require './lib/converter'
 
 RSpec.describe Converter do
   describe '#Converts' do
-    it 'from_c_to_k' do
-      expect(Converter.new.convert(432.23, 'C', 'K')).to eq [705.23, 'K']
+    context 'from_c_to_k' do
+      it { expect(Converter.new.convert(432.23, 'C', 'K')).to eq [705.23, 'K'] }
     end
-    it 'from_c_to_f' do
-      expect(Converter.new.convert(432.23, 'C', 'F')).to eq [810.01, 'F']
+
+    context 'from_c_to_f' do
+      it { expect(Converter.new.convert(432.23, 'C', 'F')).to eq [810.01, 'F'] }
     end
-    it 'from_k_to_c' do
-      expect(Converter.new.convert(432.23, 'K', 'C')).to eq [159.23, 'C']
+
+    context 'from_k_to_c' do
+      it { expect(Converter.new.convert(432.23, 'K', 'C')).to eq [159.23, 'C'] }
     end
-    it 'from_k_to_f' do
-      expect(Converter.new.convert(432.23, 'K', 'F')).to eq [319.01, 'F']
+
+    context 'from_k_to_f' do
+      it { expect(Converter.new.convert(432.23, 'K', 'F')).to eq [319.01, 'F'] }
     end
-    it 'from_f_to_c' do
-      expect(Converter.new.convert(432.23, 'F', 'C')).to eq [222.35, 'C']
+
+    context 'from_f_to_c' do
+      it { expect(Converter.new.convert(432.23, 'F', 'C')).to eq [222.35, 'C'] }
     end
-    it 'from_f_to_k' do
-      expect(Converter.new.convert(432.23, 'F', 'K')).to eq [495.13, 'K']
+
+    context 'from_f_to_k' do
+      it { expect(Converter.new.convert(432.23, 'F', 'K')).to eq [495.13, 'K'] }
     end
-    it 'from_c_to_c' do
-      expect(Converter.new.convert(432.23, 'C', 'C')).to eq [432.23, 'C']
+
+    context 'from_c_to_c' do
+      it { expect(Converter.new.convert(432.23, 'C', 'C')).to eq [432.23, 'C'] }
     end
-    it 'from_k_to_k' do
-      expect(Converter.new.convert(432.23, 'K', 'K')).to eq [432.23, 'K']
+
+    context 'from_k_to_k' do
+      it { expect(Converter.new.convert(432.23, 'K', 'K')).to eq [432.23, 'K'] }
     end
-    it 'from_f_to_f' do
-      expect(Converter.new.convert(432.23, 'F', 'F')).to eq [432.23, 'F']
+
+    context 'from_f_to_f' do
+      it { expect(Converter.new.convert(432.23, 'F', 'F')).to eq [432.23, 'F'] }
     end
   end
 end
